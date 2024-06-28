1b4f6784ea883ef307c163cdff04aa0c0975490e
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-28 17:00:12 +0300

    passes test 2

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const employee = {
   streetAddress: '12 Seattle',
 }
 
+//create a function. create a copy of employee that you will use to carry the tests.
+//The copy9newObject will prevent mutability of your original object.
 function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newObject= {... employee}
+  newObject[key] = value;
 
-  employee[key] = value;
-
-  return employee;
+  return newObject;
 }
 
 const employees = updateEmployeeWithKeyAndValue (employee, "streetAddress", "11 Broadway");
+
+console.log(employees)
+console.log(employee)
