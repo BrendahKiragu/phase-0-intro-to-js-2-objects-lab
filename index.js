ad7d6c5a2fb8b7229c15b2da52cc535870bf28c2
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-28 17:09:33 +0300

    passes test 3

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const employee = {
   streetAddress: '12 Seattle',
 }
 
-//create a function. create a copy of employee that you will use to carry the tests.
-//The copy9newObject will prevent mutability of your original object.
+//test 1: creates a function. create a copy of employee that you will use to carry the tests.
+//The copy(newObject) will prevent mutability of your original object.
 function updateEmployeeWithKeyAndValue(employee, key, value) {
   const newObject= {... employee}
   newObject[key] = value;
@@ -13,7 +13,15 @@ function updateEmployeeWithKeyAndValue(employee, key, value) {
   return newObject;
 }
 
+//test 2: return a clone of original object with new value in the streetAddress key
 const employees = updateEmployeeWithKeyAndValue (employee, "streetAddress", "11 Broadway");
+
+//test 3: creates  a function that allows mutability of original object(employee)
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
+  employee[key] = value;
+  return employee;
+}
+
 
 console.log(employees)
 console.log(employee)
