d66b2ded32913924e1c87ac543709ff7c1d0d08a
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-28 16:43:48 +0300

    passes test 1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+const employee = {
+  name: 'Sam',
+  streetAddress: '12 Seattle',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+  employee[key] = value;
+
+  return employee;
+}
+
+const employees = updateEmployeeWithKeyAndValue (employee, "streetAddress", "11 Broadway");
